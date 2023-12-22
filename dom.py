092d229368f689f8d6640c3229fad13b503a8862
Kamila Ciężabka <kamilaciezabka@gmail.com>
2023-12-23 00:31:47 +0100

    trials

--- dom.py
+++ dom.py
@@ -16,19 +16,31 @@ class Dom:
             self.matrices[key][mask == 1] = 1
 
     def build(self):
-        return self.matrices
+        return self.matrices.values()
+
+    def build2(self):
+        result = np.zeros((100, 100))
+        result[0:35, 0:55] = self.matrices["I"]
+        result[0:35, 55:100] = self.matrices["II"]
+        result[35:65, 0:55] = self.matrices["III"]
+        result[35:65, 55:100] = self.matrices["IV"]
+        result[65:100, 0:55] = self.matrices["V"]
+        result[65:100, 55:100] = self.matrices["VI"]
+        return result
+
 
 if __name__ == '__main__':
 
     params = {"indexes": {"I": [(0, 0), (0, 55), (35, 0), (35, 55)],
                           "II": [(0, 55), (0, 100), (35, 55), (35, 100)],
                           "III": [(35, 0), (35, 55), (65, 0), (65, 55)],
-                          "IV": [(50, 55), (50, 100), (65, 55), (65, 100)],
+                          "IV": [(50, 55), (50, 100), (80, 55), (65, 100)],
                           "V": [(65, 0), (65, 55), (100, 0), (100, 55)],
                           "VI": [(65, 55), (65, 100), (100, 55), (100, 100)]},
               "rozdzielczosc": {},
               "mask": {"I": 1, "II": 0, "III": 1, "IV": 1, "V": 1, "VI": 1}}
 
     dom = Dom(params)
-    print(dom.build())
-
+    print(dom.build2())
+    plt.imshow(dom.build2())
+    plt.show()
